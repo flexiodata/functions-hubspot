7c7583bc65a64f18099c1cf7aab03c080eddcb34
Aaron Williams <aaron@flex.io>
2019-11-26 13:03:38 -0600

    added pagination to list contact function

--- hubspot-list-contacts.py
+++ hubspot-list-contacts.py
@@ -10,22 +10,25 @@
 #     description: The properties to return (defaults to all properties). See "Notes" for a listing of the available properties.
 #     required: false
 # examples:
+#   - ' '
+#   - '"*"'
+#   - '"first_name, last_name, phone, email"'
 # notes: |
 #   The following properties are available:
-#     * `first_name`: first name of the person
-#     * `last_name`: last name of the person
-#     * `email`: email address of the person
-#     * `phone`: phone number of the person
-#     * `phone_mobile`: mobile phone number of the person
-#     * `job_title`: job title of the person
-#     * `address`: address of the person
-#     * `city`: city in which the person is located
-#     * `state`: state in which the person is located
-#     * `zip`: zip code in which the person is located
-#     * `country`: country in which the person is located
-#     * `linkedin_bio`: biography of the person on LinkedIn
-#     * `created_date`: date the record for this person was created
-#     * `modified_date`: last date the record for this person was modified
+#     * `first_name`: first name of the contact
+#     * `last_name`: last name of the contact
+#     * `email`: email address of the contact
+#     * `phone`: phone number of the contact
+#     * `phone_mobile`: mobile phone number of the contact
+#     * `job_title`: job title of the contact
+#     * `address`: address of the contact
+#     * `city`: city in which the contact is located
+#     * `state`: state in which the contact is located
+#     * `zip`: zip code in which the contact is located
+#     * `country`: country in which the contact is located
+#     * `linkedin_bio`: biography of the contact on LinkedIn
+#     * `created_date`: date the record for this contact was created
+#     * `modified_date`: last date the record for this contact was modified
 # ---
 
 import json
@@ -84,50 +87,84 @@ def flexio_handler(flex):
     property_map['created_date'] = 'createdate'
     property_map['modified_date'] = 'lastmodifieddate'
 
+    # list of this function's properties we'd like to query
+    properties = [p.lower().strip() for p in input['properties']]
+
+    # if we have a wildcard, get all the properties
+    if len(properties) == 1 and properties[0] == '*':
+        properties = list(property_map.keys())
+
+    # map the list of requested properties to hubspot properties; if none are
+    # available, include a blank placeholder
+    mapped_properties = [property_map.get(p,'') for p in properties]
+
+    # get the results
+    result = []
+    result.append(properties)
+
+    cursor_id = None
+    page_idx, page_max = 0, 100
+    while True:
+
+        page_result = getTablePage(auth_token, mapped_properties, cursor_id)
+        cursor_id = page_result['cursor']
+        result += page_result['data']
+
+        page_idx = page_idx + 1
+        if page_idx >= page_max or cursor_id is None:
+            break
+
+    # return the results
+    result = json.dumps(result, default=to_string)
+    flex.output.content_type = "application/json"
+    flex.output.write(result)
+
+def getTablePage(auth_token, properties, cursor_id):
+
+    # see here for more info:
+    # https://developers.hubspot.com/docs/methods/contacts/get_contacts
+
     try:
 
-        # list of this function's properties we'd like to query
-        properties = [p.lower().strip() for p in input['properties']]
-
-        # if we have a wildcard, get all the properties
-        if len(properties) == 1 and properties[0] == '*':
-            properties = list(property_map.keys())
-
-        # list of the HubSpot properties we'd like to query
-        hubspot_properties = [property_map[p] for p in properties]
-
-        # see here for more info:
-        # https://developers.hubspot.com/docs/methods/contacts/get_contacts
+        # make the request
         url_query_params = {
             'hapikey': auth_token,
             'count': 100,
             'property': ''
         }
-        url_query_str = urllib.parse.urlencode(url_query_params)
-        properties_str = "&property=".join(hubspot_properties)
-        url = 'https://api.hubapi.com/contacts/v1/lists/all/contacts/all?' + url_query_str + properties_str
+        if cursor_id is not None:
+            url_query_params['vidOffset'] = cursor_id
 
-        # get the response data as a JSON object
+        request_properties = [
+            'firstname','lastname','email','phone','mobilephone','jobtitle', 'address',
+            'city','state','zip','country','linkedinbio','createdate', 'lastmodifieddate'
+        ]
+
+        url_query_str = urllib.parse.urlencode(url_query_params)
+        url_query_str += "&property=" + "&property=".join(request_properties)
+        url = 'https://api.hubapi.com/contacts/v1/lists/all/contacts/all?' + url_query_str
+
+        # get the response
         response = requests.get(url)
+        response.raise_for_status()
         content = response.json()
 
-        # return the info
-        result = []
-        result.append(properties)
-
-        # build up each row and append it to the result
+        # get the data and the next cursor
+        data = []
         contacts = content.get('contacts',[])
         for item in contacts:
-            row = [item.get('properties').get(p,{}).get('value','') or '' for p in hubspot_properties]
-            result.append(row)
+            row = [item.get('properties').get(p,{}).get('value','') or '' for p in properties]
+            data.append(row)
 
-        # return the results
-        result = json.dumps(result, default=to_string)
-        flex.output.content_type = "application/json"
-        flex.output.write(result)
+        has_more = content.get('has-more', False)
+        next_cursor_id = content.get('vid-offset')
+        if has_more is False:
+            next_cursor_id = None
+
+        return {"data": data, "cursor": next_cursor_id}
 
     except:
-        raise RuntimeError
+        return {"data": [], "cursor": None}
 
 def validator_list(field, value, error):
     if isinstance(value, str):

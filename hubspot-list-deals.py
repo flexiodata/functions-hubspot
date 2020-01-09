84baf05a31175a0f9114bafb5711505393b521a1
Aaron Williams <aaron@flex.io>
2020-01-09 16:07:35 -0600

    added pagination support for deal listing

--- hubspot-list-deals.py
+++ hubspot-list-deals.py
@@ -72,73 +72,125 @@ def flexio_handler(flex):
         raise ValueError
 
     # map this function's property names to the API's property names
+    def convertTimestamp(ts):
+        if ts is None:
+            return ''
+        return datetime.utcfromtimestamp(int(ts)/1000).strftime('%Y-%m-%d %H:%M:%S')
     property_map = OrderedDict()
-    property_map['deal_name'] = 'dealname'
-    property_map['deal_owner'] = 'hubspot_owner_id'
-    property_map['deal_state'] = 'dealstage'
-    property_map['deal_type'] = 'dealtype'
-    property_map['amt'] = 'amount'
-    property_map['amt_home'] = 'amount_in_home_currency'
-    property_map['closed_lost_reason'] = 'closed_lost_reason'
-    property_map['closed_won_reason'] = 'closed_won_reason'
-    property_map['close_date'] = 'closedate'
-    property_map['description'] = 'description'
-    property_map['pipeline'] = 'pipeline'
-    property_map['contacts_cnt'] = 'num_associated_contacts'
-    property_map['sales_activities_cnt'] = 'num_notes'
-    property_map['times_contacted_cnt'] = 'num_contacted_notes'
-    property_map['last_contacted_date'] = 'notes_last_contacted'
-    property_map['next_activity_date'] = 'notes_next_activity_date'
-    property_map['created_date'] = 'createdate'
-    property_map['updated_date'] = 'notes_last_updated'
+    property_map['portal_id'] = lambda item: str(item.get('portalId',''))
+    property_map['deal_id'] = lambda item: str(item.get('dealId',''))
+    property_map['deal_name'] = lambda item: item.get('properties',{}).get('dealname',{}).get('value','')
+    property_map['deal_owner'] = lambda item: str(item.get('properties',{}).get('hubspot_owner_id',{}).get('value',''))
+    property_map['deal_state'] = lambda item: item.get('properties',{}).get('dealstage',{}).get('value','')
+    property_map['deal_type'] = lambda item: item.get('properties',{}).get('dealtype',{}).get('value','')
+    property_map['amt'] = lambda item: item.get('properties',{}).get('amount',{}).get('value','')
+    property_map['amt_home'] = lambda item: item.get('properties',{}).get('amount_in_home_currency',{}).get('value','')
+    property_map['closed_lost_reason'] = lambda item: item.get('properties',{}).get('closed_lost_reason',{}).get('value','')
+    property_map['closed_won_reason'] = lambda item: item.get('properties',{}).get('closed_won_reason',{}).get('value','')
+    property_map['close_date'] = lambda item: convertTimestamp(item.get('properties',{}).get('closedate',{}).get('value',None))
+    property_map['description'] = lambda item: item.get('properties',{}).get('description',{}).get('value','')
+    property_map['pipeline'] = lambda item: item.get('properties',{}).get('pipeline',{}).get('value','')
+    property_map['contacts_cnt'] = lambda item: item.get('properties',{}).get('num_associated_contacts',{}).get('value','')
+    property_map['sales_activities_cnt'] = lambda item: item.get('properties',{}).get('num_notes',{}).get('value','')
+    property_map['times_contacted_cnt'] = lambda item: item.get('properties',{}).get('num_contacted_notes',{}).get('value','')
+    property_map['last_contacted_date'] = lambda item: convertTimestamp(item.get('properties',{}).get('notes_last_contacted',{}).get('value',None))
+    property_map['next_activity_date'] = lambda item: convertTimestamp(item.get('properties',{}).get('notes_next_activity_date',{}).get('value',None))
+    property_map['created_date'] = lambda item: convertTimestamp(item.get('properties',{}).get('createdate',{}).get('value',None))
+    property_map['updated_date'] = lambda item: convertTimestamp(item.get('properties',{}).get('notes_last_updated',{}).get('value',None))
+
+    # list of this function's properties we'd like to query
+    properties = [p.lower().strip() for p in input['properties']]
+
+    # if we have a wildcard, get all the properties
+    if len(properties) == 1 and properties[0] == '*':
+        properties = list(property_map.keys())
+
+    # map the list of requested properties to hubspot properties; if none are
+    # available, include a blank placeholder
+    mapped_properties = [property_map.get(p, lambda item: '') for p in properties]
+
+    # get the results
+    result = []
+    result.append(properties)
+
+    cursor_id = None
+    page_idx, page_max = 0, 100
+    while True:
+
+        page_result = getTablePage(auth_token, mapped_properties, cursor_id)
+        cursor_id = page_result['cursor']
+        result += page_result['data']
+
+        page_idx = page_idx + 1
+        if page_idx >= page_max or cursor_id is None:
+            break
+
+    # return the results
+    result = json.dumps(result, default=to_string)
+    flex.output.content_type = "application/json"
+    flex.output.write(result)
+
+def getTablePage(auth_token, properties, cursor_id):
+
+    # see here for more info:
+    # https://developers.hubspot.com/docs/methods/deals/get-all-deals
 
     try:
-
-        # list of this function's properties we'd like to query
-        properties = [p.lower().strip() for p in input['properties']]
-
-        # if we have a wildcard, get all the properties
-        if len(properties) == 1 and properties[0] == '*':
-            properties = list(property_map.keys())
-
-        # list of the HubSpot properties we'd like to query
-        hubspot_properties = [property_map[p] for p in properties]
-
-        # see here for more info:
-        # https://developers.hubspot.com/docs/methods/deals/get-all-deals
 
         headers = {
             'Authorization': 'Bearer ' + auth_token,
         }
         url_query_params = {
-            'limit': 100,
-            'properties': ''
+            'limit': 250
         }
+        if cursor_id is not None:
+            url_query_params['offset'] = cursor_id
+
         url_query_str = urllib.parse.urlencode(url_query_params)
-        properties_str = "&properties=".join(hubspot_properties)
+        properties_str = "&properties=" + "&properties=".join([
+            'dealname',
+            'hubspot_owner_id',
+            'dealstage',
+            'dealtype',
+            'amount',
+            'amount_in_home_currency',
+            'closed_lost_reason',
+            'closed_won_reason',
+            'closedate',
+            'description',
+            'pipeline',
+            'num_associated_contacts',
+            'num_notes',
+            'num_contacted_notes',
+            'notes_last_contacted',
+            'notes_next_activity_date',
+            'createdate',
+            'notes_last_updated'
+            ])
         url = 'https://api.hubapi.com/deals/v1/deal/paged?' + url_query_str + properties_str
 
-        # get the response data as a JSON object
+        # get the response
         response = requests.get(url, headers=headers)
+        response.raise_for_status()
         content = response.json()
 
-        # return the info
-        result = []
-        result.append(properties)
+        # get the data and the next cursor
+        data = []
+        results = content.get('deals',[])
 
-        # build up each row and append it to the result
-        deals = content.get('deals',[])
-        for item in deals:
-            row = [item.get('properties').get(p,{}).get('value','') or '' for p in hubspot_properties]
-            result.append(row)
+        for result_info in results:
+            row = [p(result_info) or '' for p in properties]
+            data.append(row)
 
-        # return the results
-        result = json.dumps(result, default=to_string)
-        flex.output.content_type = "application/json"
-        flex.output.write(result)
+        has_more = content.get('hasMore', False)
+        next_cursor_id = content.get('offset')
+        if has_more is False:
+            next_cursor_id = None
+
+        return {"data": data, "cursor": next_cursor_id}
 
     except:
-        raise RuntimeError
+        return {"data": [], "cursor": None}
 
 def validator_list(field, value, error):
     if isinstance(value, str):

73ca3971d3d0cd6f161b99ee289bf213ff4e1b8f
Aaron Williams <aaron@flex.io>
2020-06-10 10:50:12 -0500

    updated status codes used to trigger request retry

--- hubspot-contacts.py
+++ hubspot-contacts.py
@@ -137,7 +137,7 @@ def get_data(params):
 def requests_retry_session(
     retries=3,
     backoff_factor=0.3,
-    status_forcelist=(500, 502, 504),
+    status_forcelist=(429, 500, 502, 503, 504),
     session=None,
 ):
     session = session or requests.Session()
